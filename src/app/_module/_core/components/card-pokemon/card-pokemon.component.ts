6f0bfc58d7da3a2c2a9cd65eb9ff83591097f425
FlpEmag <153771379+FlpEmag@users.noreply.github.com>
2024-02-14 22:16:28 -0300

    Card-Pokemon

---
+++ src/app/_module/_core/components/card-pokemon/card-pokemon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'card-pokemon',
+    templateUrl: './card-pokemon.component.html',
+    styleUrl: './card-pokemon.component.scss',
+})
+
+export class CardPokemonComponent {}
--- src/app/_module/_core/core.module.ts
+++ src/app/_module/_core/core.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from "@angular/core";
 import { CorePage } from "./page/core.page";
 import { BrowserModule } from "@angular/platform-browser";
+import { CardPokemonComponent } from "./components/card-pokemon/card-pokemon.component";
 
 @NgModule({
-    declarations:[CorePage],
-    imports:[BrowserModule],
-    bootstrap:[CorePage],
+    declarations:[
+        CorePage,
+        CardPokemonComponent,
+    ],
+    imports:[
+        BrowserModule
+    ],
+    bootstrap:[
+        CorePage
+    ],
 })
 export class CoreModule {}
